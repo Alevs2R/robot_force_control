b51bd2e1df1c54169d961aa8db03f38ee25eaa9d
alevs2r <a.evlampev@dooglys.com>
2018-11-25 21:40:04 +0300

    add more charts (have problems in formulas)

--- dynamics.py
+++ dynamics.py
@@ -28,19 +28,27 @@ def getqdotdot(t):
     return 2 * pi * (qf-q0) * sin (2*pi*t/tau) / tau
 
 
-def getM(q):
+def getM(q_):
+    qc = q_[0]
     return np.array([
-        [J1 + J2 + m * (l ** 2) + m * (q[1] ** 2) - m * 2 * l**2 / (sin(q[0])**2) * (1 - cos(q[0])), 0],
-        [0, -m*l + m * 2 * l * (1 - cos(q[0])) / (sin(q[0]) ** 2)]
+        [J1 + J2 + 2 * m * (l ** 2) / (sin(qc)**2) * (2 * (1 - cos(qc)) ** 2 - (1 - cos(qc))), 0],
+        [0, -m*l*(1 + 2*(1-cos(qc))/sin(qc)**2)]
     ])
 
 
 
 def getC(q, dq):
+    qc = q[0]
+    dq = dq[0]
     return np.array([
-        2 * q[1] * dq[1] * m * dq[0] - m * (l**2) * 2 / (sin(q[0]) ** 3) * (dq[0]**2) * (1 - cos(q[0]))**2,
-        m * 2 * l / (sin(q[0])**3) * dq[0]**2 * (1-cos(q[0]))**2
+        6 * dq**2 * m * l**2 * (1-cos(qc))**2 / sin(qc)**3,
+        - m * 2 * dq**2 * l / sin(qc) * ((1 - cos(qc)) + (1 - cos(qc))**2 / sin(qc)**2)
     ])
+
+    # return np.array([
+    #     2 * q[1] * dq[1] * m * dq[0] - m * (l**2) * 2 / (sin(q[0]) ** 3) * (dq[0]**2) * (1 - cos(q[0]))**2,
+    #     m * 2 * l / (sin(q[0])**3) * dq[0]**2 * (1-cos(q[0]))**2 - m * dq[0]**2 * q[1]
+    # ])
 
 
 def getG(q):
@@ -88,12 +96,12 @@ forces = np.zeros(shape=(2, time.shape[0]))
 for (i,), cur_time in np.ndenumerate(time):
     forces[:, i] = getM(q[:, i]).dot(q2dot[:, i]) + getC(q[:, i], qdot[:, i]) # + getG(q[:, i])
 
-plt.xlabel('time, ms')
+plt.xlabel('time, s')
 plt.title('force M')
 plt.plot(time, forces[0, :])
 plt.show()
 
-plt.xlabel('time, ms')
+plt.xlabel('time, s')
 plt.title('force P')
 plt.plot(time, forces[1, :])
 plt.show()
@@ -116,28 +124,43 @@ for (i,), cur_time in np.ndenumerate(time):
     M_inv = np.linalg.inv(getM(q_current))
     q2dot_real[:, i-1] = M_inv.dot(u - c_current - g_current)
     qdot_real[:, i] = qdot_real[:, i-1] + q2dot_real[:, i-1] * dt
-    q_real[:, i] = q_real[:, i-1] + (qdot_real[:, i] + qdot_real[:, i-1]) / 2 * dt
+    q_real[:, i] = q_real[:, i-1] + qdot_real[:, i-1] * dt + q2dot_real[:, i-1] * dt**2 / 2
 
-plt.xlabel('time, ms')
+plt.xlabel('time, s')
 plt.title('q1')
 plt.plot(time, q[0, :], 'g')
 plt.plot(time, q_real[0, :], 'r')
 plt.show()
 
-plt.xlabel('time, ms')
+plt.xlabel('time, s')
 plt.title('q2')
 plt.plot(time, q[1, :], 'g')
 plt.plot(time, q_real[1, :], 'r')
 plt.show()
 
-plt.xlabel('time, ms')
-plt.title('q2 velocity')
-plt.plot(time, qdot[1, :], 'g')
+plt.xlabel('time, s')
+plt.title('q1 velocity')
+plt.plot(time, qdot[0, :], 'g')
+plt.plot(time, qdot_real[0, :], 'r')
 plt.show()
 
-plt.xlabel('time, ms')
+plt.xlabel('time, s')
+plt.title('q2 velocity')
+plt.plot(time, qdot[1, :], 'g')
+plt.plot(time, qdot_real[1, :], 'r')
+plt.show()
+
+plt.xlabel('time, s')
+plt.title('q1 acceleration')
+plt.plot(time, q2dot[0, :], 'g')
+plt.plot(time, q2dot_real[0, :], 'r')
+plt.show()
+
+plt.xlabel('time, s')
 plt.title('q2 acceleration')
 plt.plot(time, q2dot[1, :], 'g')
+plt.plot(time, q2dot_real[1, :], 'r')
 plt.show()
+
 
 
